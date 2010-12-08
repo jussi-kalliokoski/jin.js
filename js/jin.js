0bbdc2b17095386f56a97854122d5f1574dbb277
Jussi Kallliokoski <jussi.kalliokoski@gmail.com>
2010-12-08 13:02:03 +0000

    Initial import

---
+++ js/jin.js
@@ -0,0 +1,229 @@
+var Jin;
+(function (document, window){
+
+	function adapt(original, modifier)
+	{
+		if (typeof modifier == 'string')
+			return original + new Number(modifier.substr(1));
+		return modifier;
+	}
+
+	function extend(obj)
+	{
+		var i, n;
+		for (i=1; i<arguments.length; i++)
+			for (n in arguments[i])
+				obj[n] = arguments[i][n];
+	}
+
+	function appendChildren(parent)
+	{
+		var i;
+		for (i=1; i<arguments.length; i++)
+			parent.appendChild(arguments[i]);
+	}
+
+	function hasClass(elem, cl)
+	{
+		var classes, i, n, hasClass, elems = (typeof elem == 'array') ? elem : [elem];
+		for (i=0; i < elems.length; i++)
+		{
+			classes = elems[i].className.split(' ');
+			for (n=0; n < classes.length; n++)
+				if (classes[n] == cl)
+					return true;
+		}
+		return false;
+	}
+
+	function hasClasses(elem, cls)
+	{
+		var cl = cls, i;
+		if (typeof cl == 'string')
+			cl = cl.split(' ');
+		for (i=0; i<cl.length; i++)
+			if (!hasClasses)
+				return false;
+		return true;
+	}
+
+	function addClass(elem, cl)
+	{
+		var classes, i, n, hasClass, elems = (typeof elem == 'array') ? elem : [elem];
+		for (i=0; i < elems.length; i++)
+		{
+			hasClass = false;
+			if (elems[i].className.length == 0)
+				classes = [];
+			else
+				classes = elems[i].className.split(' ');
+			for (n=0; n < classes.length; n++)
+				if (classes[n] == cl)
+				{
+					hasClass = true;
+					break;
+				}
+			if (!hasClass)
+				classes.push(cl);
+			elems[i].className = classes.join(' ');
+		}
+	}
+
+	function addClasses(elem, cls)
+	{
+		var cl = cls, i;
+		if (typeof cl == 'string')
+			cl = cl.split(' ');
+		for (i=0; i<cl.length; i++)
+			addClass(elem, cl[i]);
+	}
+
+	function removeClass(elem, cl)
+	{
+		var classes, i, n, hasClass, elems = (elem.length) ? elem : [elem];
+		for (i=0; i < elems.length; i++)
+		{
+			hasClass = false;
+			if (elems[i].className.length == 0)
+				classes = [];
+			else
+				classes = elems[i].className.split(' ');
+			for (n=0; n < classes.length; n++)
+				if (classes[n] == cl)
+					classes.splice(n--, 1);
+			elems[i].className = classes.join(' ');
+		}
+	}
+
+	function removeClasses(elem, cls)
+	{
+		var cl = cls, i;
+		if (typeof cl == 'string')
+			cl = cl.split(' ');
+		for (i=0; i<cl.length; i++)
+			removeClass(elem, cl[i]);
+	}
+
+	function bind(elem, type, func, pass)
+	{
+		var fnc = function(e)
+		{
+			e.data = pass;
+			func(e);
+		}
+		elem.addEventListener(type, fnc, false);
+		if (!elem._binds)
+			elem._binds = [];
+		elem._binds.push({type: type, func: func, fnc: fnc});
+	}
+
+	function unbind(elem, type, func)
+	{
+		if (elem._binds)
+		for (var i=0; i<elem._binds.length; i++)
+			if (elem._binds[i].type == type && elem._binds[i].func == func)
+				elem.removeEventListener(type, elem._binds[i].fnc, false);
+			
+	}
+
+	function getOffset(elem, parent)
+	{
+		var pElement = elem, top = 0, left = 0;
+		while (pElement && pElement != parent)
+		{
+			left += pElement.offsetLeft;
+			top += pElement.offsetTop;
+			left -= pElement.scrollLeft;
+			top -= pElement.scrollTop;
+			pElement = pElement.parentNode;
+		}
+		return {x: left, y: top};
+	}
+
+	// Element grabber
+	var grab;
+	(function(){
+		grab = function(elem, options)
+		{
+			var data =
+			{
+				move: {x: 0, y: 0},
+				offset: {x: 0, y: 0},
+				position: {x: 0, y: 0},
+				start: {x: 0, y: 0},
+				affects: document,
+				stopPropagation: true,
+				preventDefault: true,
+				touch: false // Not implemented yet
+			}
+			extend(data, options);
+			bind(elem, 'mousedown', mousedown, data);
+		}
+		function mousedown(e)
+		{
+			e.data.position.x = e.pageX;
+			e.data.position.y = e.pageY;
+			e.data.start.x = e.pageX;
+			e.data.start.y = e.pageY;
+			e.data.event = e;
+			if (e.data.onstart && e.data.onstart(e.data))
+				return;
+			if (e.preventDefault && e.data.preventDefault)
+				e.preventDefault();
+			if (e.stopPropagation && e.data.stopPropagation)
+				e.stopPropagation();
+			bind(e.data.affects, 'mousemove', mousemove, e.data);
+			bind(e.data.affects, 'mouseup', mouseup, e.data);
+		}
+		function mousemove(e)
+		{
+			if (e.preventDefault && e.data.preventDefault)
+				e.preventDefault();
+			if (e.stopPropagation && e.data.stopPropagation)
+				e.stopPropagation();
+			e.data.move.x = e.pageX - e.data.position.x;
+			e.data.move.y = e.pageY - e.data.position.y;
+			e.data.position.x = e.pageX;
+			e.data.position.y = e.pageY;
+			e.data.offset.x = e.pageX - e.data.start.x;
+			e.data.offset.y = e.pageY - e.data.start.y;
+			e.data.event = e;
+			if (e.data.onmove)
+				e.data.onmove(e.data);
+		}
+		function mouseup(e)
+		{
+			if (e.preventDefault && e.data.preventDefault)
+				e.preventDefault();
+			if (e.stopPropagation && e.data.stopPropagation)
+				e.stopPropagation();
+			unbind(e.data.affects, 'mousemove', mousemove);
+			unbind(e.data.affects, 'mouseup', mouseup);
+			e.data.event = e;
+			if (e.data.onfinish)
+				e.data.onfinish(e.data);
+		}
+	})();
+
+	function getContentSize(elem)
+	{
+		// Make something up here.
+	}
+
+	Jin = {
+		bind: bind,
+		unbind: unbind,
+		grab: grab,
+		addClasses: addClasses,
+		addClass: addClass,
+		removeClasses: removeClasses,
+		removeClass: removeClass,
+		hasClasses: hasClasses,
+		hasClass: hasClass,
+		getOffset: getOffset,
+		adapt: adapt,
+		extend: extend,
+		appendChildren: appendChildren,
+		getContentSize: getContentSize
+	};
+})(document, window);
---
+++ js/jindow.js
@@ -0,0 +1,196 @@
+var Jindow;
+
+(function(document, window){
+	Jindow = function(options)
+	{
+		if (!Jin)
+			throw('Jin-library is missing!');
+		var opts = 
+		{
+			title: 'New Window',
+			buttonCaptions: ['X', '+', '-', '_'],
+			state: 0,
+			left: 50,
+			top: 0,
+			width: 400,
+			height: 300,
+			closeButton: true,
+			minButton: true,
+			resizable: true,
+			visible: true,
+			onclose: null
+		}
+		wnd = new function JindowWindow()
+		{
+			extend(this, opts, options);
+			var that = this,
+			window = createDiv(),
+			titlebar = createDiv(),
+			contentbox = createDiv(),
+			controlbox = createDiv(),
+			menubox = createDiv(),
+			titlebox = createDiv(),
+			btnClose = createElem('button'),
+			btnMaxRes = createElem('button'),
+			btnMinimize = createElem('button');
+
+			appendChildren(window, titlebar, contentbox);
+			appendChildren(titlebar, titlebox, menubox, controlbox);
+			appendChildren(controlbox, btnMinimize, btnMaxRes, btnClose);
+
+			addClass(window, 'window');
+			addClass(titlebar, 'titlebar');
+			addClass(contentbox, 'content');
+			addClass(controlbox, 'controls');
+			addClass(menubox, 'menu');
+			addClass(titlebox, 'title');
+			addClass(btnClose, 'close');
+			addClass(btnMaxRes, 'maximize');
+			addClass(btnMinimize, 'minimize');
+
+			btnClose.title = 'Close';
+			btnMinimize.title = 'Minimize';
+
+			bind(btnClose, 'click', close);
+			grab(titlebox, {
+				onstart: function(e){ addClass(document.body, 'moving'); },
+				onmove: function(e){ move('='+e.move.x, '='+e.move.y); },
+				onfinish: function(e){ removeClass(document.body, 'moving'); }
+			});
+			grab(window, {
+				onstart: function(e)
+				{
+					var rightSide = (e.position.x > that.left + that.width - 5),
+					bottomSide = (e.position.y > that.top + that.height - 5);
+					if (rightSide && bottomSide)
+						addClass(document.body, 'resizing');
+					else
+						return true;
+				},
+				onmove: function(e)
+				{
+					resize('='+e.move.x, '='+e.move.y);
+				},
+				onfinish: function(e)
+				{
+					removeClass(document.body, 'resizing');
+				}
+			});
+
+			refresh();
+
+			this.dom = window;
+			this.body = contentbox;
+			this.refresh = refresh;
+			this.close = close;
+			this.minimize = minimize;
+			this.maximize = maximize;
+			this.move = move;
+			this.resize = resize;
+
+			function refresh()
+			{
+				titlebox.innerHTML = that.title;
+				switch(that.state)
+				{
+					case 0:
+						
+						break;
+					case 1:
+						break;
+					case 2:
+						break;
+					case 3:
+						break;
+				}
+				window.style['display'] = (that.visible && that.state != 4) ? 'block' : 'none';
+				window.style['left'] = that.left+'px';
+				window.style['top'] = that.top+'px';
+				window.style['width'] = that.width+'px';
+				window.style['height'] = that.height+'px';
+				btnClose.style['display'] = (that.closeButton) ? 'inline' : 'none';
+				btnMinimize.style['display'] = (that.minButton) ? 'inline' : 'none';
+				btnMaxRes.style['display'] = (that.resizable) ? 'inline' : 'none';
+				btnClose.innerHTML = that.buttonCaptions[0];
+				btnMaxRes.innerHTML = (that.state != 3) ? that.buttonCaptions[1] : that.buttonCaptions[2];
+				btnMinimize.innerHTML = that.buttonCaptions[3];
+			}
+
+			function close(force)
+			{
+				if (that.onclose && !that.onclose() && force !== true)
+					return;
+				for (var i=0; i<windows.length; i++)
+					if (windows[i] == that)
+						windows.splice(i--, 1);
+				window.parentNode.removeChild(window);
+				that = null;
+			}
+
+			function minimize()
+			{
+				
+			}
+
+			function maximize()
+			{
+				
+			}
+
+			function move(left, top)
+			{
+				if (left !== null)
+				{
+					that.left = adapt(that.left, left);
+					window.style['left'] = that.left+'px';
+				}
+				if (top !== null)
+				{
+					that.top = adapt(that.top, top);
+					window.style['top'] = that.top+'px';
+				}
+			}
+
+			function resize(width, height)
+			{
+				if (width !== null)
+				{
+					that.width = adapt(that.width, width);
+					window.style['width'] = that.width+'px';
+				}
+				if (height != null)
+				{
+					that.height = adapt(that.height, height);
+					window.style['height'] = that.height+'px';
+				}
+			}
+		};
+		return wnd;
+	};
+
+	var
+		bind = Jin.bind,
+		unbind = Jin.unbind,
+		grab = Jin.grab,
+		addClasses = Jin.addClasses,
+		addClass = Jin.addClass,
+		removeClasses = Jin.removeClasses,
+		removeClass = Jin.removeClass,
+		hasClasses = Jin.hasClasses,
+		hasClass = Jin.hasClass,
+		getOffset = Jin.getOffset,
+		adapt = Jin.adapt,
+		extend = Jin.extend,
+		appendChildren = Jin.appendChildren
+	;
+
+	function createDiv()
+	{
+		return createElem('div');
+	}
+
+	function createElem(type)
+	{
+		return document.createElement(type);
+	}
+})(document, window);
