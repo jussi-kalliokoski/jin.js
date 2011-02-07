8626234b7a94cdf79089e0a193ece65e087ff9bf
jussi-kalliokoski <jussi.kalliokoski@gmail.com>
2011-02-07 11:02:32 +0200

    Cleaned up some swipe

--- js/jin-swipe.js
+++ js/jin-swipe.js
@@ -8,39 +8,37 @@
 	Jin('swipe', swipe);
 	Jin('angularSwipe', angularSwipe);
 
-	function startSwipe(swipeObj)
-	{
+	function startSwipe(swipeObj){
 		var swipe = {};
 		extend(swipe, swipeObj);	
 		swipe.swipeObj = swipeObj;
 		swipeObj.motions.push(swipe);
-		swipe.kill = function()
-		{
+		swipe.kill = function(){
 			clearTimeout(this.timer);
-			if (this.onEnd)
+			if (this.onEnd){
 				this.onEnd(this.passData);
-			for (var i=0; i<swipeObj.motions.length; i++)
-				if (swipeObj.motions[i] == this)
+			}
+			for (var i=0; i<swipeObj.motions.length; i++){
+				if (swipeObj.motions[i] == this){
 					swipeObj.motions.splice(i--, 1);
+				}
+			}
 		}
 		return swipe;
 	}
 
-	function unswipe(elem)
-	{
+	function unswipe(elem){
 		ungrab(elem);
-		for (var i=0; i<swipeObjs.length; i++)
-			if (swipeObjs[i].area == elem)
-			{
+		for (var i=0; i<swipeObjs.length; i++){
+			if (swipeObjs[i].area == elem){
 				swipeObjs[i].kill();
 				swipeObjs.splice(i--, 1);
 			}
+		}
 	}
 
-	function swipe(elem, arg1, arg2, arg3, arg4)
-	{
-		var swipeObj =
-		{
+	function swipe(elem, arg1, arg2, arg3, arg4){
+		var swipeObj = {
 			onMotion: function(){},
 			onSwipe: undefined,
 			onEnd: undefined,
@@ -50,30 +48,30 @@
 			interval: 50,
 			multiple: false
 		};
-		if (typeof arg1 == 'object')
+		if (typeof arg1 == 'object') {
 			extend(swipeObj, arg1);
-		else if (typeof arg1 == 'function')
-		{
+		} else if (typeof arg1 == 'function') {
 			swipeObj.onMotion = arg1;
-			if (typeof arg2 == 'object')
+			if (typeof arg2 == 'object') {
 				extend(swipeObj, arg2);
-			else if(typeof arg2 == 'function')
-			{
+			} else if(typeof arg2 == 'function') {
 				swipeObj.onSwipe = arg2;
-				if (typeof arg3 == 'object')
+				if (typeof arg3 == 'object'){
 					extend(swipeObj, arg3);
-				else if (typeof arg3 == 'function')
-				{
+				} else if (typeof arg3 == 'function'){
 					swipeObj.onEnd = arg3;
-					if (typeof arg4 == 'object')
+					if (typeof arg4 == 'object'){
 						extend(swipeObj, arg4);
+					}
 				}
 			}
 		}
-		else
+		else {
 			throw 'Invalid arguments.';
-		if (swipeObj.friction <= 1)
+		}
+		if (swipeObj.friction <= 1){
 			throw 'Invalid arguments: Friction must be over 1.';
+		}
 		extend(swipeObj,
 		{
 			previousAngle: undefined,
@@ -85,55 +83,53 @@
 			released: true,
 			timer: undefined,
 			motions: [],
-			kill: function()
-			{
-				for (var i=0; i<this.motions.length; i++)
+			kill: function(){
+				for (var i=0; i<this.motions.length; i++){
 					this.motions[i].kill();
+				}
 			}
 		});
 		swipeObjs.push(swipeObj);
-		grab(elem, {
-			onstart: function(e)
-			{
-				if (e.data.onSwipe && !e.data.onSwipe.apply(e.data.area, [e, e.data.passData]))
+		grab(elem, {onstart: function(e){
+				if (e.data.onSwipe && !e.data.onSwipe.apply(e.data.area, [e, e.data.passData])){
 					return true;
-				if (!e.data.multiple && e.data.motions.length)
+				}
+				if (!e.data.multiple && e.data.motions.length){
 					e.data.motions[0].kill();
+				}
 				var swipe = startSwipe(e.data);
 				swipe.motionX = 0;
 				swipe.motionY = 0;
 				swipe.released = false;
-				swipe.timer = setInterval(function()
-				{
-					if (swipe.motionX == 0 && swipe.motionY == 0 && swipe.released)
+				swipe.timer = setInterval(function(){
+					if (swipe.motionX == 0 && swipe.motionY == 0 && swipe.released){
 						return swipe.kill();
+					}
 					swipe.onMotion.apply(swipe.area, [swipe.motionX, swipe.motionY, swipe.passData]);
 					swipe.motionX /= swipe.friction;
 					swipe.motionY /= swipe.friction;
-					if (swipe.motionX < 0.1 && swipe.motionX > -0.1)
+					if (swipe.motionX < 0.1 && swipe.motionX > -0.1){
 						swipe.motionX = 0;
-					if (swipe.motionY < 0.1 && swipe.motionY > -0.1)
+					}
+					if (swipe.motionY < 0.1 && swipe.motionY > -0.1){
 						swipe.motionY = 0;
+					}
 				}, swipe.interval);
 				e.data = swipe;
 			},
-			onmove: function(e)
-			{
+			onmove: function(e){
 				e.data.motionX += e.move.x / 10;
 				e.data.motionY += e.move.y / 10;
 			},
-			onfinish: function(e)
-			{
+			onfinish: function(e){
 				e.data.released = true;
 			},
 			data: swipeObj
 		});
 	};
 
-	function angularSwipe(elem, arg1, arg2, arg3, arg4)
-	{
-		var swipeObj =
-		{
+	function angularSwipe(elem, arg1, arg2, arg3, arg4){
+		var swipeObj = {
 			onMotion: function(){},
 			onSwipe: undefined,
 			onEnd: undefined,
@@ -145,20 +141,17 @@
 			centerX: undefined,
 			centerY: undefined
 		};
-		if (typeof arg1 == 'object')
+		if (typeof arg1 == 'object'){
 			extend(swipeObj, arg1);
-		else if (typeof arg1 == 'function')
-		{
+		} else if (typeof arg1 == 'function') {
 			swipeObj.onMotion = arg1;
-			if (typeof arg2 == 'object')
+			if (typeof arg2 == 'object'){
 				extend(swipeObj, arg2);
-			else if(typeof arg2 == 'function')
-			{
+			} else if(typeof arg2 == 'function') {
 				swipeObj.onSwipe = arg2;
-				if (typeof arg3 == 'object')
+				if (typeof arg3 == 'object'){
 					extend(swipeObj, arg3);
-				else if (typeof arg3 == 'function')
-				{
+				} else if (typeof arg3 == 'function') {
 					swipeObj.onEnd = arg3;
 					if (typeof arg4 == 'object')
 						extend(swipeObj, arg4);
@@ -169,8 +162,7 @@
 			throw 'Invalid arguments.';
 		if (swipeObj.friction <= 1)
 			throw 'Invalid arguments: Friction must be over 1.';
-		extend(swipeObj,
-		{
+		extend(swipeObj, {
 			motionX: 0,
 			motionY: 0,
 			area: elem,
@@ -180,16 +172,14 @@
 			circulateX: undefined,
 			circulateY: undefined,
 			previousAngle: undefined,
-			kill: function()
-			{
+			kill: function(){
 				for (var i=0; i<this.motions.length; i++)
 					this.motions[i].kill();
 			}
 		});
 		swipeObjs.push(swipeObj);
 		grab(elem, {
-			onstart: function(e)
-			{
+			onstart: function(e){
 				if (e.data.onSwipe && !e.data.onSwipe.apply(e.data.area, [e, e.data.passData]))
 					return true;
 				if (!e.data.multiple && e.data.motions.length)
@@ -220,8 +210,7 @@
 				}, swipe.interval);
 				e.data = swipe;
 			},
-			onmove: function(e)
-			{
+			onmove: function(e){
 				var newAngle = Math.atan2(e.position.y - e.data.circulateY, e.position.x - e.data.circulateX),
 				angleDiff = newAngle - e.data.previousAngle;
 				if (angleDiff > Math.PI)
@@ -231,8 +220,7 @@
 				e.data.motion += angleDiff;
 				e.data.previousAngle = newAngle;
 			},
-			onfinish: function(e)
-			{
+			onfinish: function(e){
 				e.data.released = true;
 			},
 			data: swipeObj
